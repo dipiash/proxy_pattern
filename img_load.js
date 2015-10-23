5f093ec249b79e74207e3a6b94097fe102e2750c
dipiash <dipiash@users.noreply.github.com>
2015-10-23 20:55:48 +0300

    Added comments. Renamed variable

--- img_load.js
+++ img_load.js
@@ -5,21 +5,28 @@
   function setView(itemSelector) {
     var scrollTop = $( window ).scrollTop(),
         windowHeight = $( window ).height(),
-        elementsCache = $( itemSelector );
+        elementsCache = $( itemSelector ); // получаем все виртуальные объекты
 
     elementsCache.each(function() {
       var element = $( this ),
-          offsetTop = element.offset().top;
+          elementOffsetTop = element.offset().top;
 
-      if (scrollTop + windowHeight >= offsetTop) {
+      if (scrollTop + windowHeight >= elementOffsetTop) { // если виртуальный
+        // объект попадает в область видимости, то
+        // создает реальный объект
         element.attr('src', element.attr('data-src'));
+        // удалемя информацию о том, что это виртуальный объект.
         element.removeClass('preloader');
       }
     });
   }
 
+  // Проверяем, а нет ли при загрузке страницы виртуального объекта в области
+  // видимости
   setView('.img__item.preloader');
 
+  // Проверяем, а нет ли при скроллинге страницы виртуального объекта в области
+  // видимости
   $(window).scroll(function() {
     setView('.img__item.preloader');
   });
