4276af66d51f74f3bcfd3b0f3563109a3374463d
Dmitriy <dipiash@users.noreply.github.com>
2023-09-10 18:21:24 +0300

    Update img_load.js

--- img_load.js
+++ img_load.js
@@ -1,32 +1,28 @@
-// вирутальный заместитель. Тяжелые объекты загружаются по требованию
+// virtual deputy. Heavy objects are loaded on demand
 
 ;(function() {
 
   function setView(itemSelector) {
     var scrollTop = $( window ).scrollTop(),
         windowHeight = $( window ).height(),
-        elementsCache = $( itemSelector ); // получаем все виртуальные объекты
+        elementsCache = $( itemSelector ); // get all virtual objects
 
     elementsCache.each(function() {
       var element = $( this ),
           elementOffsetTop = element.offset().top;
 
-      if (scrollTop + windowHeight >= elementOffsetTop) { // если виртуальный
-        // объект попадает в область видимости, то
-        // создает реальный объект
+      if (scrollTop + windowHeight >= elementOffsetTop) { // if a virtual object comes into scope, it creates a real object
         element.attr('src', element.attr('data-src'));
-        // удалемя информацию о том, что это виртуальный объект.
+        // removing information that this is a virtual object.
         element.removeClass('preloader');
       }
     });
   }
 
-  // Проверяем, а нет ли при загрузке страницы виртуального объекта в области
-  // видимости
+  // Checking if there is a virtual object in the scope when loading the page
   setView('.img__item.preloader');
 
-  // Проверяем, а нет ли при скроллинге страницы виртуального объекта в области
-  // видимости
+  // Checking if there is a virtual object in the scope when scrolling the page
   $(window).scroll(function() {
     setView('.img__item.preloader');
   });
